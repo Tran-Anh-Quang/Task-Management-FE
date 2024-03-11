bb953511736b029b5f78c64d9520071c07dbe9ae
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-03-12 00:42:42 +0700

    feat:added navbar

---
+++ src/page/navbar/NavBar.jsx
@@ -0,0 +1,19 @@
+import { Avatar } from '@mui/material';
+import React from 'react';
+import "./NavBar.css"
+
+const NavBar = () => {
+    return (
+        <div className='container z-10 sticky left-0 right-0 top-0 py-3 px-5 
+        lg:px-10, flex justify-between items-center'>
+            <p className='font-bold text-lg'>Dino Task Manager</p>
+
+            <div className='flex items-center gap-5'>
+                <p>Welcome back, Dino</p>
+                <Avatar>C</Avatar>
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
