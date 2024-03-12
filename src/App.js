4d7a9233ed1e17f76abf4f6837527fa031bbef11
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-03-12 14:02:26 +0700

    feat:added side bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { ThemeProvider } from "@emotion/react";
 import { darkTheme } from "./theme/darktheme";
 import NavBar from "./page/navbar/NavBar";
+import Home from "./page/home/Home";
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       
       <NavBar />
+      <Home />
 
     </ThemeProvider>
   );
